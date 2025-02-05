2a99c3cce9fbdffa33da263dff73c3a1a320d879
Aspira GT <aspiragt@gmail.com>
2025-02-05 12:53:45 -0600

    Fixed frontend dependencies and routing issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
-import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Experiences from './pages/Experiences';
 import RedeemGift from './pages/RedeemGift';
 
 function App() {
   return (
-    <ChakraProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/experiences" element={<Experiences />} />
-          <Route path="/redeem" element={<RedeemGift />} />
-          <Route path="/redeem/:code" element={<RedeemGift />} />
-        </Routes>
-      </Router>
-    </ChakraProvider>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/redeem" element={<RedeemGift />} />
+        <Route path="/redeem/:code" element={<RedeemGift />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
-import App from './App.jsx'
-import theme from './theme'
-import './index.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import App from './App'
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: '#E6FFFA',
+      100: '#B2F5EA',
+      200: '#81E6D9',
+      300: '#4FD1C5',
+      400: '#38B2AC',
+      500: '#319795',
+      600: '#2C7A7B',
+      700: '#285E61',
+      800: '#234E52',
+      900: '#1D4044',
+    },
+  },
+  fonts: {
+    heading: '"Inter", sans-serif',
+    body: '"Inter", sans-serif',
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
